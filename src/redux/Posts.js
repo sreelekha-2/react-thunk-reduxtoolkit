9d6125ade3e515a3c0f5943ea069b1595b96b091
sreelekha-2 <sreelekhakutikanti@gmail.com>
2022-08-01 18:07:59 +0530

    1 file modified

--- src/redux/Posts.js
+++ src/redux/Posts.js
@@ -12,6 +12,14 @@ export default function Posts() {
   return (
     <div>
       <h1>Posts Component</h1>
+      <ul>
+        {posts.map((post) => (
+          <li>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
